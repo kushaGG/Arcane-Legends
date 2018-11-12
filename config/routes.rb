b5d9d1785f73d83ee6c035789e5554f7a165dcf4
AK <artem.kushniruk29@gmail.com>
2018-11-12 21:55:56 +0200

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  resources :characters
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
